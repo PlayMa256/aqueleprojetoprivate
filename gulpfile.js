7941de53792ddbdbf645c4de5cd267bbb192fcfd
Matheus Silva <matheus.g.silva@hotmail.com>
2015-12-27 15:37:39 +0000

    testando na cloud9

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('gulp-autoprefixer');
-var lost = require('lost');
+// var postcss = require('gulp-postcss');
+// var autoprefixer = require('gulp-autoprefixer');
+// var lost = require('lost');
 
-gulp.task('postcss', function () {
-	var processors = [
-		autoprefixer,
-		lost
-	];
-  return gulp
-    .src("sass/*.scss")
-    .pipe(sass())
-    .pipe(postcss(processors))
-    .pipe(gulp.dest("dist/css"));
-});
+// gulp.task('postcss', function () {
+// 	var processors = [
+// 		autoprefixer,
+// 		lost
+// 	];
+//   return gulp
+//     .src("sass/*.scss")
+//     .pipe(sass())
+//     .pipe(postcss(processors))
+//     .pipe(gulp.dest("dist/css"));
+// });
 
 gulp.task('sass', function(){
-	return gulp.src("sass/*.scc")
+	return gulp.src("sass/*.scss")
 	.pipe(sass())
 	.pipe(gulp.dest("dist/css"));
 });
 
 gulp.task('watch:styles', function(){
-	gulp.watch('sass/*.scss', ['postcss']);
-	//gulp.watch('sass/*.scss', ['sass']);
+	//gulp.watch('sass/*.scss', ['postcss']);
+	gulp.watch('sass/*.scss', ['sass']);
 });
