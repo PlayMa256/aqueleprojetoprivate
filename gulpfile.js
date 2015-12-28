556665cae5cba333084e27f2a9d4e6d06df148c5
Matheus Silva <matheus.g.silva@hotmail.com>
2015-12-28 23:15:30 +0000

    resolvendo problema da disposicao do conteudo

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-// var postcss = require('gulp-postcss');
-// var autoprefixer = require('gulp-autoprefixer');
-// var lost = require('lost');
+var autoprefixer = require('gulp-autoprefixer');
 
-// gulp.task('postcss', function () {
-// 	var processors = [
-// 		autoprefixer,
-// 		lost
-// 	];
-//   return gulp
-//     .src("sass/*.scss")
-//     .pipe(sass())
-//     .pipe(postcss(processors))
-//     .pipe(gulp.dest("dist/css"));
-// });
 
 gulp.task('sass', function(){
 	return gulp.src("sass/*.scss")
 	.pipe(sass())
+	.pipe(autoprefixer())
 	.pipe(gulp.dest("dist/css"));
 });
 
